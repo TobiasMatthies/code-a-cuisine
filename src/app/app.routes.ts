e4be0243d773c8fdc5512529016f23dfa04d8dab
TobiasMatthies <tbsmatthies@gmail.com>
2025-10-09 11:18:52 -0300

    added layout of generate recipe page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
+import { GenerateRecipe } from './generate-recipe/generate-recipe';
 import { Welcome } from './welcome/welcome';
 
 export const routes: Routes = [
   {
     path: '',
     component: Welcome,
+  },
+  {
+    path: 'generate-recipe',
+    component: GenerateRecipe
   }
 ];
