d115caf1fe549417d0beb512aafd272bcee91c17
TobiasMatthies <tbsmatthies@gmail.com>
2025-10-14 11:56:26 -0300

    implemented loading screen

--- src/app/new-recipes/preferences/preferences.ts
+++ src/app/new-recipes/preferences/preferences.ts
@@ -1,17 +1,24 @@
 import { TitleCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LoadingScreen } from '../loading-screen/loading-screen';
 
 @Component({
   selector: 'app-preferences',
-  imports: [RouterModule, TitleCasePipe],
+  imports: [RouterModule, TitleCasePipe, LoadingScreen],
   templateUrl: './preferences.html',
   styleUrl: './preferences.css'
 })
 export class Preferences {
+  isLoading = false;
+
   preferences = {
     times: ['Quick', 'Medium', 'Complex'],
     cuisine: ['german', 'italian', 'indian', 'japanese', 'gourmet', 'fusion'],
     dietPrefences: ['vegetarian', 'vegan', 'keto', 'no preferences']
+  }
+
+  onGenerateRecipe() {
+    this.isLoading = true
   }
 }
