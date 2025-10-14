7892efc6535b3c803a054cb59e87b9dae2e0df15
TobiasMatthies <tbsmatthies@gmail.com>
2025-10-14 11:38:30 -0300

    implemented preferences page

--- src/app/new-recipes/preferences/preferences.ts
+++ src/app/new-recipes/preferences/preferences.ts
@@ -1,12 +1,17 @@
+import { TitleCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-preferences',
-  imports: [RouterModule],
+  imports: [RouterModule, TitleCasePipe],
   templateUrl: './preferences.html',
   styleUrl: './preferences.css'
 })
 export class Preferences {
-
+  preferences = {
+    times: ['Quick', 'Medium', 'Complex'],
+    cuisine: ['german', 'italian', 'indian', 'japanese', 'gourmet', 'fusion'],
+    dietPrefences: ['vegetarian', 'vegan', 'keto', 'no preferences']
+  }
 }
